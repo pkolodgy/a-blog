270f35b6453ea8a2aa6567de87f4a8fc85ae80eb
Patrick Kolodgy <email": null>
2016-12-29 14:16:24 -0500

    Registration errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :username, uniqueness: true
   has_secure_password
+  validates :password, length: { minimum: 6 }
 end
