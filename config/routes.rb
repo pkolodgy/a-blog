7f6c3cc91f0de97166b2c0ec6301e2f605e53ac4
Patrick Kolodgy <email": null>
2016-12-29 15:14:53 -0500

    Login routes and methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   resources :users, :only => [:new, :create, :show]
 end
